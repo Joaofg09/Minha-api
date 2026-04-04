9d1e792d7a9649dc89ed5e00e3eb85714459d357
João <joaofg0912@gmail.com>
2026-04-04 20:15:17 -0300

    fix: porta dinâmica para deploy

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use('/auth', require('./routes/auth'))
 app.use('/tarefas', require('./routes/tarefas'))
 
-app.listen(3000, () => {
-  console.log('API rodando em http://localhost:3000')
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`)
 })
